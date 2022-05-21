6a6816ae5a0047206cd8a5326ada2b248fc064b9
lgl <2508664769@qq.com>
2022-05-21 21:09:20 +0800

    Branch01 is created!

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello lgl!");
         System.out.println("This is for lab6 test.");
+        System.out.println("Branch01 is created!");
     }
 }
